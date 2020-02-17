d249e4b17c65d7d0b1100850f9bbcbf30504e3c1
Nyi Nyi Zaw <nyinyizaw@supers.team>
2020-02-17 17:50:31 +0630

    add menu item in home page

--- app/src/main/java/com/example/todoexample/MainActivity.kt
+++ app/src/main/java/com/example/todoexample/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.todoexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,6 +11,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_home,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        return super.onOptionsItemSelected(item)
 
     }
 }
